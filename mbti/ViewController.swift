b61aa96552e1e22ffc35879b2bba730dd36250c8
Josie Chung <josie.chung@goodfinance.com>
2024-07-16 11:30:00 +0800

    Initial Commit

---
+++ mbti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mbti
+//
+//  Created by Josie.Chung on 2024/7/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
